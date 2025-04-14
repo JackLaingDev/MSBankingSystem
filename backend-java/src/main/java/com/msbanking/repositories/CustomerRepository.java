9a0a9e34d0543f013197396ace9fb33a305782f4
Jack <jacklaing1234@gmail.com>
2025-04-14 18:02:35 +0100

    Added findByUsername() to CustomerRepository

--- backend-java/src/main/java/com/msbanking/repositories/CustomerRepository.java
+++ backend-java/src/main/java/com/msbanking/repositories/CustomerRepository.java
@@ -4,5 +4,5 @@ import com.msbanking.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    Customer findByCustomerID(int customerID);
+    Customer findByUsername(String username);
 }
