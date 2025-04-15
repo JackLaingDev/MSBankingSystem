fbe6c3dad910f1560766e80a8c9bedcd9d23c1af
Jack <jacklaing1234@gmail.com>
2025-04-15 18:42:43 +0100

    Very basic initial setup of react frontend

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
