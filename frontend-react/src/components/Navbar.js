cee7309476ab1c2bfe3c87df9f86f0c728797517
Jack <jacklaing1234@gmail.com>
2025-04-16 16:50:43 +0100

    Added conditional render to Accounts in Navbar

--- frontend-react/src/components/Navbar.js
+++ frontend-react/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ isLoggedIn }) {
   return (
     <AppBar position="static">
       <Toolbar>
@@ -10,7 +10,9 @@ function Navbar() {
           JLBank
         </Typography>
         <Button color="inherit" component={Link} to="/home">Home</Button>
-        <Button color="inherit" component={Link} to="/accounts">Accounts</Button>
+        {isLoggedIn && ( // Only show if logged in
+          <Button color="inherit" component={Link} to="/accounts">Accounts</Button>
+        )}
         <Button color="inherit" component={Link} to="/login">Login</Button>
       </Toolbar>
     </AppBar>
