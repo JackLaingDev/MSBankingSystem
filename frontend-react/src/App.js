1a0433897600aca6d124b985d20c3fe5094919c8
Jack <jacklaing1234@gmail.com>
2025-04-16 15:55:51 +0100

    Imported Home to App

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
