c4592312faeedb7efd23a989e7cf155ddf8a075a
Jack <jacklaing1234@gmail.com>
2025-04-16 15:56:42 +0100

    Added very basic navbar boilerplate

---
+++ frontend-react/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          MSBank
+        </Typography>
+        <Button color="inherit" component={Link} to="/home">Home</Button>
+        <Button color="inherit" component={Link} to="/accounts">Accounts</Button>
+        <Button color="inherit" component={Link} to="/login">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
