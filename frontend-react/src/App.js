5c0c81b472a123652b246f0fe772fefbbb76ff0c
Jack <jacklaing1234@gmail.com>
2025-04-16 15:54:42 +0100

    Added home page route

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
