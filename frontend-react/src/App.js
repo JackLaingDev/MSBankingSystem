3f8421666b5446bdc5987efdd0a43dbc6bc6e490
Jack <jacklaing1234@gmail.com>
2025-04-16 16:49:38 +0100

    Added isLoggedIn state to App

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,12 +5,14 @@ import Login from './components/Login';
 import Home from './pages/Home';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
       </Routes>
     </Router>
   );
