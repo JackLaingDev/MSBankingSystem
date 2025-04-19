cfc4917727e21cd8b999b1957a744f0dc9970793
Jack <jacklaing1234@gmail.com>
2025-04-19 14:16:45 +0100

    Accounts now use grid system, improves appearance

--- frontend-react/src/pages/Account.js
+++ frontend-react/src/pages/Account.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Typography, Button, Box } from '@mui/material';
+import { Container, Typography, Button, Box, Paper, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Accounts({ isLoggedIn, customerID }) {
@@ -16,31 +16,44 @@ function Accounts({ isLoggedIn, customerID }) {
 
   const openAccount = async () => {
     const newAcc = {
-      customer: { customerID: customerID },
+      customer: { customerID },
       accountType: 0,
       balance: 0
     };
     await axios.post('http://localhost:8080/api/accounts/create', newAcc);
-    // Refresh accounts list
     const res = await axios.get(`http://localhost:8080/api/customers/${customerID}/accounts`);
     setAccounts(res.data);
   };
 
   return (
-    <>
-      <Container>
-        <Typography variant="h5" gutterBottom>My Accounts</Typography>
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h5" gutterBottom>My Accounts</Typography>
+
+      <Grid container spacing={3}>
         {accounts.map((acc) => (
-          <Box key={acc.accountID} mb={2}>
-            <Typography>Account ID: {acc.accountID}</Typography>
-            <Typography>Balance: £{acc.balance}</Typography>
-            <Typography>Type: {acc.accountType}</Typography>
-          </Box>
+          <Grid item xs={12} sm={6} md={4} key={acc.accountID}>
+            <Paper sx={{ p: 3 }}>
+              <Typography>Account ID: {acc.accountID}</Typography>
+              <Typography>Balance: £{acc.balance}</Typography>
+              <Typography>Type: {acc.accountType}</Typography>
+              <Button
+                variant="outlined"
+                component={Link}
+                to={`/accounts/${acc.accountID}`}
+                sx={{ mt: 1 }}
+              >
+                View
+              </Button>
+            </Paper>
+          </Grid>
         ))}
+      </Grid>
+
+      <Box mt={4} display="flex" gap={2}>
         <Button variant="contained" onClick={openAccount}>Open New Account</Button>
         <Button variant="contained" color="inherit" component={Link} to="/transfer">Transfer</Button>
-      </Container>
-    </>
+      </Box>
+    </Container>
   );
 }
 
